97c861df306af6865cba5dc0c9f6a53e057eef04
Jonathan <icaiwei@icloud.com>
2020-11-19 11:54:15 +0800

    print中的中文修改为英文，避免docker系统输出报错 新增requirements 新增测试数据

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 def train(**kwargs):
     config = Config()
     config.update(**kwargs)
-    print('当前设置为:\n', config)
+    print('current config:\n', config)
     if config.use_cuda:
         torch.cuda.set_device(config.gpu)
     print('loading corpus')
