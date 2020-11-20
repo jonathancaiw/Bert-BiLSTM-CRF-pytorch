3aa7ee9e19201798bfdf2560c857e3d6a47d59ec
Jonathan <icaiwei@icloud.com>
2020-11-20 14:57:57 +0800

    数据集去重，过滤超长数据

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 from config import *
 
+conf = Config()
+
 
 def convert(filename, skip_o=True):
     x = []
@@ -38,8 +40,56 @@ def append_x_y(x, y, chars, labels, skip_o=True):
         y.append(labels)
 
 
+def distinct_dataset(x, y):
+    """
+    数据集去重
+    :param x:
+    :param y:
+    :return:
+    """
+    distinct_x = []
+    distinct_y = []
+    s = set()
+
+    for index in range(len(y)):
+        str_x = ''.join(x[index])
+        if str_x not in s:
+            s.add(str_x)
+            distinct_x.append(x[index])
+            distinct_y.append(y[index])
+
+    return distinct_x, distinct_y
+
+
+def trim_dataset(x, y, len_limit):
+    """
+    过滤掉超长数据
+    :param x:
+    :param y:
+    :param len_limit:
+    :return:
+    """
+    if not len_limit > 0:
+        return x, y
+
+    filtered_x = []
+    filtered_y = []
+
+    for index in range(len(y)):
+        if len(y[index]) <= len_limit:
+            filtered_x.append(x[index])
+            filtered_y.append(y[index])
+
+    return filtered_x, filtered_y
+
+
 def generate_dataset(filename):
     x, y = convert(filename)
+
+    x, y = distinct_dataset(x, y)
+
+    # 最大有效长度=最大长度-开始位-结束位
+    x, y = trim_dataset(x, y, conf.max_length - 2)
 
     train_x, left_x, train_y, left_y = get_train_test_dataset(x, y,
                                                               (DEV_SIZE + TEST_SIZE) / (TRAIN_SIZE + DEV_SIZE + TEST_SIZE))
